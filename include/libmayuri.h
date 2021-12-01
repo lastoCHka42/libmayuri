d7dab5ef96d6ecead73eb48669df9202fe645d64
Pavel Balaev <mail@void.so>
2021-12-02 00:01:55 +0300

    init commit

---
+++ include/libmayuri.h
@@ -0,0 +1,14 @@
+#ifndef LIBMAYURI_H_
+#define LIBMAYURI_H_
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+void lm_dummy(void);
+
+#ifdef _cplusplus
+}
+#endif
+
+#endif
---
+++ src/lm_dummy.c
@@ -0,0 +1,3 @@
+void lm_dummy()
+{
+}
